97afc2f7d9e7c69c4ddae94424ec8aaac3a3f861
mje713 <chewyuing@gmail.com>
2022-10-14 20:04:54 -0400

    Add project files.

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid id { get; set; }
+
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DataSetDateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+
+        public bool Visible { get; set; }
+    }
+}
---
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,45 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace Bloggie.Web.Pages.Admin.Blogs
+{
+    public class AddModel : PageModel
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
+
+        public AddModel(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                FeturedImageUrl = AddBlogPostRequest.FeturedImageUrl,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible
+            };
+
+            bloggieDbContext.BlogPosts.Add(blogPost);
+            bloggieDbContext.SaveChanges(); 
+        }
+
+    }
+}
